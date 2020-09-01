c5d5f5d1c2b9a2cb1453780318c194662e8b2155
David Ramirez <david.ramirez1448@gmail.com>
2020-09-01 18:08:17 -0400

    cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Deeplinks } from '@ionic-native/deeplinks/ngx';
 
 @Component({
   selector: 'app-root',
@@ -13,7 +14,8 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private deeplinks: Deeplinks,
   ) {
     this.initializeApp();
   }
@@ -21,7 +23,17 @@ export class AppComponent {
   initializeApp() {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
+      this.statusBar.backgroundColorByHexString('#ffff');
       this.splashScreen.hide();
+      this.deeplinkSetup();
     });
+  }
+
+  private deeplinkSetup(): void {
+    this.deeplinks.route({
+      '': ''
+    }).subscribe(match => {
+      alert(JSON.stringify(match));
+    })
   }
 }
